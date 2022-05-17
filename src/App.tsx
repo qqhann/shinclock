5a850e963077110548e47f372e146888341accc7
Qiushi Pan <17402261+qqhann@users.noreply.github.com>
2022-05-17 19:10:54 +0900

    feat: count up

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import { useIntervalWhen, useToggle } from "rooks";
 
 function App() {
+  const start = new Date();
+  const [diff, setDiff] = useState<number>(0);
+  const [isOn, toggle] = useToggle();
+
+  useIntervalWhen(
+    () => {
+      setDiff((old) => old + 1);
+    },
+    1000,
+    isOn,
+    true
+  );
+
   return (
     <div className="h-full w-full bg-slate-300">
       <h1 className="text-3xl">ShinClock</h1>
+      <button onClick={toggle}>Toggle</button>
+      <span>{diff}</span>
     </div>
   );
 }
