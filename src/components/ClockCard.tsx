9763d2788ac7221fd231e1acefa5295a1cf252fe
Qiushi Pan <17402261+qqhann@users.noreply.github.com>
2022-05-19 23:15:24 +0900

    feat: timer works

--- src/components/ClockCard.tsx
+++ src/components/ClockCard.tsx
@@ -1,9 +1,12 @@
-import React from "react";
-import { useIntervalWhen, useToggle } from "rooks";
+import React, { useState } from "react";
+import { useIntervalWhen } from "rooks";
+import { intervalToDuration, Duration } from "date-fns";
+
 import { Clock, useClock } from "../hooks/clock";
 
 export const ClockCard = (props: { clock: Clock }) => {
   const { clock } = props;
+  const [duration, setDuration] = useState<Duration>({});
   const { start, stop, remove } = useClock(clock.ref);
   const toggle = () => {
     if (clock.running) stop();
@@ -11,7 +14,10 @@ export const ClockCard = (props: { clock: Clock }) => {
   };
   useIntervalWhen(
     () => {
-      // countDown()
+      if (!clock.start_at) return;
+      setDuration(
+        intervalToDuration({ start: clock.start_at.toDate(), end: new Date() })
+      );
     },
     1000,
     clock.running,
@@ -25,8 +31,7 @@ export const ClockCard = (props: { clock: Clock }) => {
             {clock.name}
           </h1>
           <p className="flex-auto text-lg font-semibold text-slate-900">
-            {clock.start_at && clock.start_at.toDate().toDateString()}
-            {/* {("000" + second).slice(-2)}s */}
+            {duration.hours}:{duration.minutes}:{duration.seconds}
           </p>
         </div>
         <div className="flex space-x-4 mb-6 text-sm font-medium">
