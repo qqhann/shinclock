198f0fd0be9eb3fcc569b655c0989cc64cd54d74
Qiushi Pan <17402261+qqhann@users.noreply.github.com>
2022-05-21 16:34:44 +0900

    chore: add memo

--- src/components/FirestoreInput.tsx
+++ src/components/FirestoreInput.tsx
@@ -2,20 +2,20 @@ import React from "react";
 import { DocumentData, DocumentReference, updateDoc } from "firebase/firestore";
 import { useInput, useToggle } from "rooks";
 
-export const FirestoreInput = <
-  T extends DocumentData & { ref: DocumentReference }
->(props: {
-  doc: T;
-  field: keyof T;
-}) => {
-  const { doc, field } = props;
-  const [editing, toggle] = useToggle();
-  const inputProps = useInput(doc[field]);
-  const onBlur = () => {
-    doc && updateDoc(doc.ref, { [field]: inputProps.value });
-    toggle(editing);
-  };
+export const FirestoreInput = React.memo(
+  <T extends DocumentData & { ref: DocumentReference }>(props: {
+    doc: T;
+    field: keyof T;
+  }) => {
+    const { doc, field } = props;
+    const [editing, toggle] = useToggle();
+    const inputProps = useInput(doc[field]);
+    const onBlur = () => {
+      doc && updateDoc(doc.ref, { [field]: inputProps.value });
+      toggle(editing);
+    };
 
-  if (editing) return <input {...inputProps} onBlur={onBlur} />;
-  else return <span onClick={toggle}>{doc[field]}</span>;
-};
+    if (editing) return <input {...inputProps} onBlur={onBlur} />;
+    else return <span onClick={toggle}>{doc[field]}</span>;
+  }
+);
